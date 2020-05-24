b6d41f66910e7fc15852aa3bbe735fe1978628d9
Arsenei Abraamyan <ar.abraamyan@gmail.com>
2020-05-24 22:45:30 +0300

    add first code array

---
+++ array_example.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+using namespace std;
+
+
+
+int main()
+{
+    int arr[4];
+
+    arr[0] = 5;
+    arr[1] = 7;
+    arr[2] = 10;
+    arr[3] = 33333;
+    arr[5] = 1;
+    
+
+    cout << arr[4]<< endl;
+}
